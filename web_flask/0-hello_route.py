d57025160701ec7a8bc246f89b6d858d4ba2bf9a
iekeng <ekeng7.eb@gmail.com>
2023-05-21 01:35:18 +0100

    flask index routw

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Set strict_slashes to false in route definition.
-    Any route with or without a trailing slash will 
+    Any route with or without a trailing slash will
     trigger the specific decorated function.
 """
 
@@ -8,10 +8,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def 0-hello_route():
     """ This function displays 'Hello HBNB'
-        when the '/' route is visited on the 
+        when the '/' route is visited on the
         server
     """
+
     return 'Hello HBNB!'
