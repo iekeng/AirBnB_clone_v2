3f7efeb4a3951634e47c834a67ae383460fdabce
iekeng <ekeng7.eb@gmail.com>
2023-05-21 10:35:54 +0100

    checks for odd or even numbers passed as url params

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,65 +1,81 @@
 #!/usr/bin/python3
 """ Set strict_slashes to false in route definition.
-    Any route with or without a trailing slash will 
+    Any route with or without a trailing slash will
     trigger the specific decorated function.
 """
 
-from flask import Flask
+from flask import Flask, escape, render_template
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     """ This function displays 'Hello HBNB'
-        when the '/' route is visited on the 
+        when the '/' route is visited on the
         server.
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ This function displays `HBNB`
-        when the '/hbnb' route is visited on the 
+        when the '/hbnb' route is visited on the
         server.
     """
-    return 'HBNB!'
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text='is cool'):
     """ This function displays `C <text>`
-        when the '/c/' route is visited on the 
-        server.
+        when the '/c/' route is visited on the
+        server.  <text> default is set to 'cool'.
     """
     text = text.replace('_', ' ')
     result = "C %s" % escape(text)
     return result
 
-@app.route('/python/(<text>)', strict_slashes=False)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_text(text='cool'):
     """ This function displays `Python <text>`
-        when the '/python/' route is visited on the 
-        server.
+        when the '/python/' route is visited.
+        <text> default is set to 'cool'.
     """
     text = text.replace('_', ' ')
     result = "Python %s" % escape(text)
     return result
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """ This function displays `Python <text>`
-        when the '/number/' route is visited on the 
+    """ This function displays `<n> is a number`
+        when the '/number/' route is visited on the
         server.
     """
-    result = "%d is a number" % escape(n)
+    result = "%d is a number" % n
     return result
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ This function displays an html template
-        when the '/number_template/' route is visited on the 
+        when the '/number_template/' route is visited on the
+        server.
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def odd_even(n):
+    """ This function displays an html template
+        when the 'number_odd_or_even/' route is visited on the
         server.
     """
     return render_template('6-number_odd_or_even.html', n=n)
 
-if __name__ =='__main__'():
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
