cb4bd95e00be31e7c4ee8613028365a3109f924d
iekeng <ekeng7.eb@gmail.com>
2023-05-21 09:22:20 +0100

    /hbnb route defined

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ strict_slashes set to false in all route definitions.
-    Any route with or without a trailing slash will trigger 
+    Any route with or without a trailing slash will trigger
     the specific decorated function.
 """
 
@@ -8,21 +8,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ This function displays 'Hello HBNB'
-        when the '/' route is visited on the 
+        when the '/' route is visited on the
         server. <text> default is set to 'cool'
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ This function displays `HBNB`
-        when the '/hbnb' route is visited on the 
+        when the '/hbnb' route is visited on the
         server. <text> default is set to 'cool'.
     """
     return 'HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
