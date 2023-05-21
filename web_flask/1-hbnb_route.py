c196edf0cc78201ab25619cdf19351be0bfb1592
iekeng <ekeng7.eb@gmail.com>
2023-05-21 09:15:53 +0100

    /hbnb rout defined

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,8 +5,8 @@
 """
 
 
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
@@ -24,5 +24,5 @@ def hbnb():
     """
     return 'HBNB!'
 
-if __name__ == '__main__'():
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
