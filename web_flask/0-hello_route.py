e34c4b7b34460c99b17e575da8aca0b20ef85d3a
iekeng <ekeng7.eb@gmail.com>
2023-05-21 09:14:24 +0100

    index route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,8 +5,8 @@
 """
 
 
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
@@ -19,5 +19,5 @@ def index():
     return 'Hello HBNB!'
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
